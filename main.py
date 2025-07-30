8116c07f6326674db03505e0f7ffd9fea21dc6f8
Gaurav44sonar <ssonar44477@gmail.com>
2025-07-31 01:59:55 +0530

    Solved Errors again again444444444444

--- main.py
+++ main.py
@@ -201,4 +201,4 @@ async def shutdown_event():
     print("Cleanup completed")
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=int(os.environ.get("PORT", 8000)), reload=False)
+    uvicorn.run("main:app", host="0.0.0.0", port=int(os.environ.get("PORT", 800)), reload=False)
