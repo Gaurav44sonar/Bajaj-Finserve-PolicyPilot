0f94f2610fdbd7827f97a6895c04f7226dbb6cc5
Gaurav44sonar <ssonar44477@gmail.com>
2025-07-31 18:10:42 +0530

    Final project but not complete

--- authorization.py
+++ authorization.py
@@ -1,6 +1,6 @@
 # authorization.py
 
-API_KEY = "supersecretkey12"  # Replace with your actual secret token
+API_KEY = "e459c094dbdc72e9366e2645b8ab14f5c2039de3b0ab118f581d7b60d2044923"  # Replace with your actual secret token
 
 def validate_token(auth_header: str):
     if not auth_header or not auth_header.startswith("Bearer "):
